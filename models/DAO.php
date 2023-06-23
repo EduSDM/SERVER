5566b3201544bef57b6c92331588afe3baad0eee
EduSDM <edu675314@gmail.com>
2023-06-23 12:27:19 -0300

    first commit

---
+++ models/DAO.php
@@ -0,0 +1,12 @@
+<?php
+
+interface DAO
+{
+    public function create($data);
+    public function read();
+    public function update($id, $data);
+    public function delete($id);
+
+}
+
+?>
---
+++ models/Database.php
@@ -0,0 +1,56 @@
+<?php
+
+class Database
+{
+    private string $host;
+    private string $database;
+    private string $username;
+    private string $password;
+    private $conn;
+
+
+    public function __construct($host, $database, $username, $password)
+    {
+
+        $this->$host = $host;
+        $this->$database = $database;
+        $this->username = $username;
+        $this->password = $password;
+    }
+    public function connect()
+    {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+        //verificar conexão e alertar erro
+
+        if ($this->conn->connect_error) {
+            die("Error in connection of database") . $this->conn->connect_error;
+        }
+
+    }
+
+    public function query($sql)
+    {
+        $results = $this->conn->query($sql);
+        if (!$results) {
+            die("Error in access sql: " . $this->conn->error);
+        }
+        return $results;
+    }
+
+    public function close()
+    {
+        $this->conn->close();
+    }
+    public function getError()
+    {
+        return $this->conn->error;
+    }
+    public function getLastInsertedId()
+    {
+        return $this->conn->insert_id;
+    }
+
+}
+
+?>
